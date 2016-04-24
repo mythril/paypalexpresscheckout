a79ac325e586d117b23a6a213a31c5a445f3b567
Lucas Green <lucas.m.green@gmail.com>
2016-04-23 19:53:58 -0700

    Fleshed out payment result a bit more

--- src/PaymentResult.php
+++ src/PaymentResult.php
@@ -4,7 +4,33 @@ namespace Mythril\PayPal\ExpressCheckout;
 
 class PaymentResult {
 	protected $data;
+	protected $transactionId;
+	protected $paymentStatus;
+	protected $pendingReason;
 	public function __construct(array $data) {
 		$this->data = $data;
+		$this->transactionId = $data['PAYMENTINFO_0_TRANSACTIONID'];
+		$this->paymentStatus = $data['PAYMENTINFO_0_PAYMENTSTATUS'];
+		$this->pendingReason = $data['PAYMENTINFO_0_PENDINGREASON'];
+	}
+
+	public function __get($key) {
+		return $this->data[$key];
+	}
+
+	public function getData() {
+		return $this->data;
+	}
+
+	public function getTransactionId() {
+		return $this->transactionId;
+	}
+
+	public function getPaymentStatus() {
+		return $this->paymentStatus;
+	}
+
+	public function getPendingReason() {
+		return $this->pendingReason;
 	}
 }
